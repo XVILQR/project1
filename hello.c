5a18cc1dbfcb1ab661b7e19faab16ad648ba6ee9
daifei <2418636690@qq.com>
2023-07-08 12:22:31 +0800

    add void test()

--- hello.c
+++ hello.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 
+void test()
+{
+    for (size_t i = 0; i < 100; i++)
+    {
+        printf("%d\n",i*i);
+    }
+    
+
+
+
+}
+
+
 
 
 int main()
 {
-    printf("hello world!!!\n");
+    test();
     return 0;
 }
